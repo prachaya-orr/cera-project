3ef2f1ec02a349d49bdde201eb29d7c2d022b720
prachaya-orr <prachaya.orr@gmail.com>
2022-10-07 17:39:26 +0700

    fixed path addProduct

--- src/pages/Admin/AddProductPage.js
+++ src/pages/Admin/AddProductPage.js
@@ -108,7 +108,7 @@ function AddProductPage() {
             </li>
             <li>
               <Link
-                to="addProduct"
+                to="/addProduct"
                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <svg
