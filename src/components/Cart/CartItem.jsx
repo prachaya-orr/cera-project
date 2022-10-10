761add00ec18d74e95c19c2fe322193231acdefe
prachaya-orr <prachaya.orr@gmail.com>
2022-10-10 19:04:40 +0700

    done My CartUI

---
+++ src/components/Cart/CartItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CartItem() {
+  return (
+    <div className="py-[24px] w-[80vh] h-[288px] bg-red-600 border-y-2 border-gray-400">
+      CartItem
+    </div>
+  );
+}
+
+export default CartItem;
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CartItem from '../components/Cart/CartItem';
 
 function ProductPage() {
   return (
@@ -11,12 +12,101 @@ function ProductPage() {
         <div className="flex flex-col items-center p-0 gap-[108px] w-[100vw]">
           {/* Container */}
           <div className="flex flex-col items-center p-0 gap-[108px]">
-            {/* LogoName */}
             <p className="lg-h3 text-center tracking-[-0.02em]">My Cart</p>
-            {/* close LogoName */}
-            {/* CardProduct */}
-            <div className=" container 2xl flex flex-row flex-wrap justify-center items-center py-0 px-[88px] gap-[24px]">
-              Pending.....
+            <div className="flex flex-col items-center py-0 px-[176px] ">
+              <div className="flex flex-col items-start p-0 gap-6 w-[80vw]">
+                <div className="flex flex-col justify-center items-start p-0 h-[288px] gap-[24px]  w-[100%] border-y-2 border-gray-400">
+                  <div className="flex flex-row justify-around p-0 gap-12 min-w-full h-[240px]">
+                    <div className="w-[180px] h-[240px]">
+                      <img
+                        src="https://res.cloudinary.com/dtyk9alph/image/upload/v1665385383/1665385380479557716445.jpg"
+                        alt="photoImage"
+                        className="min-h-full"
+                      />
+                    </div>
+                    <div className="flex flex-col justify-between items-start py-[40px] gap-[24px] w-[50vw] bg-white">
+                      <div className="w-[100%] bg-white h-[45%] flex flex-row justify-between">
+                        <div>
+                          <div>MOCK1</div>
+                          <div className="text-gray-500">ROOKIE Collection</div>
+                          <div className="text-gray-500">Size : L</div>
+                        </div>
+                        <div>
+                          <div>
+                            <p className="lg-subtitle-2">
+                              THB &nbsp; &nbsp;&nbsp;1,690
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-[100%] bg-white h-[20%] flex flex-row justify-between">
+                        <div className="flex flex-row gap-4 items-center">
+                          <button className="p-2  bg-slate-200 rounded">
+                            -
+                          </button>
+                          <p className="">1</p>
+                          <button className="p-2 bg-slate-200 rounded">
+                            +
+                          </button>
+                        </div>
+                        <div className="text-gray-500 underline">Remove</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center p-0 gap-6 w-[80vw]">
+                <div className="flex flex-col justify-center items-start p-0 h-[288px] gap-[24px] border-b-2 w-[100%] border-gray-400">
+                  <div className="flex flex-row justify-around p-0 gap-12 min-w-full h-[240px]">
+                    <div className="w-[180px] h-[240px]">
+                      <img
+                        src="https://res.cloudinary.com/dtyk9alph/image/upload/v1665385383/1665385380479557716445.jpg"
+                        alt="photoImage"
+                        className="min-h-full"
+                      />
+                    </div>
+
+                    <div className="flex flex-col justify-between items-start py-[40px] gap-[24px] w-[50vw] bg-white">
+                      <div className="w-[100%] bg-white h-[45%] flex flex-row justify-between">
+                        <div>
+                          <div>MOCK1</div>
+                          <div className="text-gray-500">ROOKIE Collection</div>
+                          <div className="text-gray-500">Size : L</div>
+                        </div>
+                        <div>
+                          <div>
+                            <p className="lg-subtitle-2">
+                              THB &nbsp; &nbsp;&nbsp;1,690
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-[100%] bg-white h-[20%] flex flex-row justify-between">
+                        <div className="flex flex-row gap-4 items-center">
+                          <button className="p-2  bg-slate-200 rounded">
+                            -
+                          </button>
+                          <p className="">1</p>
+                          <button className="p-2 bg-slate-200 rounded">
+                            +
+                          </button>
+                        </div>
+                        <div className="text-gray-500 underline">Remove</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <button className="bg-black text-white font-bold p-5">
+                    Check Out
+                  </button>
+                  <div className="text-gray-500 underline">
+                    CONTINUE TO SHOPPING
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           {/* close Container */}
